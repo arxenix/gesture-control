2bdb128a86075a613bcd790e86c3ffde7566c642
Aashish Welling <aashishwelling@gmail.com>
2016-04-30 15:23:23 +0000

    Removed menu

--- main.js
+++ main.js
@@ -17,7 +17,8 @@ function createWindow () {
   mainWindow.loadURL('file://' + __dirname + '/index.html')
 
   // Open the DevTools.
-  mainWindow.webContents.openDevTools()
+  // mainWindow.webContents.openDevTools()
+  mainWindow.setMenu(null);
 
   // Emitted when the window is closed.
   mainWindow.on('closed', function () {
