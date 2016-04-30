d2986ded5a5d3ca5bb982cd39b1d20c6fdd0e8fa
Xenoderf <kunaalispro@gmail.com>
2016-04-30 11:41:38 -0400

    Changed icon

--- main.js
+++ main.js
@@ -11,7 +11,7 @@ let mainWindow
 
 function createWindow () {
   // Create the browser window.
-  mainWindow = new BrowserWindow({width: 800, height: 600, icon:'fa-hand-paper-o_256_0_000000_none.png'});
+  mainWindow = new BrowserWindow({width: 800, height: 600, icon:'fa-hand-paper-o_100_0_000000_none.png'});
 
   // and load the index.html of the app.
   mainWindow.loadURL('file://' + __dirname + '/index.html')
